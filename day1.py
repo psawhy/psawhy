854287b0b90f975f6f066bc60e31ab421316e1e2
unknown <psawhy2@gmail.com>
2024-02-26 16:11:37 +0330

    this is message

---
+++ day1.py
@@ -0,0 +1,21 @@
+# tain ashar posht add
+
+# print(round(8/ 3, 2))
+
+# az beynbordan ashar = 
+# print((8//3))
+
+# project day 2
+
+print("wellcome to the tip calcutor")
+bill = float(input("what was the total bill $"))
+tip=int(input("How much tip would you like to give? 10 , 12, or 15?"))
+people = int(input("How many people to split the bill?"))
+tip_as_percent = tip/200
+totla_tip_amount = bill * tip_as_percent
+totla_bill = bill + totla_tip_amount
+bill_per_person = totla_bill / people
+final_amount = round(bill_per_person , 2)
+print(f"eache persion should pay : ${final_amount}")
+
+# day3
